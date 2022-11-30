43b2563b37bdc696b264cde03fa87bbc48cf711d
ASerrai <serrai.achraf@gmail.com>
2022-11-30 13:25:35 +0100

    simplify pom and project more, remove swagger

--- src/main/java/org/acme/ressource/ExerciseResource.java
+++ src/main/java/org/acme/ressource/ExerciseResource.java
@@ -3,11 +3,6 @@ package org.acme.ressource;
 import io.vertx.ext.web.handler.HttpException;
 import org.acme.entity.Exercise;
 import org.acme.model.ExerciseDto;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
@@ -18,42 +13,21 @@ import java.util.stream.Collectors;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@Path("")
+@Path("exercises")
 public class ExerciseResource {
 
-    @Operation(summary = "Get the list of all exercises (without video)")
-    @APIResponses(value = {
-            @APIResponse(responseCode = "200", description = "Exercises returned",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ExerciseDto[].class))})
-    })
     @GET
-    @Path("/exercises")
     public Response list() {
         return Response.ok(Exercise.listAll().stream().map(ex -> new ExerciseDto((Exercise) ex)).collect(Collectors.toList())).build();
     }
 
-    @Operation(summary = "Get the exercise with the id passed as PathParam (without video)")
-    @APIResponses(value = {
-            @APIResponse(responseCode = "200", description = "Exercise returned",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ExerciseDto.class)) }),
-            @APIResponse(responseCode = "404", description = "Exercise not found in the data base",
-                    content = @Content) })
     @GET
-    @Path("/exercises/{id}")
+    @Path("/{id}")
     public Response exerciseById(Long id) {
         return Response.ok(new ExerciseDto(this.getExercise(id))).build();
     }
 
-    @Operation(summary = "Create new exercise (secured)")
-    @APIResponses(value = {
-            @APIResponse(responseCode = "200", description = "Exercise created",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ExerciseDto.class)) }),
-            @APIResponse(responseCode = "401", description = "Token passed in the header is not valid or expired",
-                    content = @Content) })
     @POST
-    @Path("/secure/exercises")
     @Transactional
     public Response create(ExerciseDto exerciseDto) {
         final Exercise exercise = exerciseDto.toEntity();
